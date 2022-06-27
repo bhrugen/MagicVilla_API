abc200253886e5a6fc7505202acd56b52d6b922f
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-06-27 11:15:36 -0500

    Section 2 - Using controller name in route

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    //[Route("api/[controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
