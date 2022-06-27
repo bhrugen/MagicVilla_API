5718bf66bad5629d125721c6f804517a19f8a8cf
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-06-27 11:24:52 -0500

    Section 2 - Villa Data Store

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +13,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<VillaDTO> {
-            new VillaDTO{Id=1,Name="Pool View" },
-            new VillaDTO{Id=2,Name="Beach View" }
-            };
+            return VillaStore.villaList;
         }
     }
 }
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+            new VillaDTO{Id=1,Name="Pool View" },
+            new VillaDTO{Id=2,Name="Beach View" }
+            };
+    }
+}
