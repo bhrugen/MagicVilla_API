6cd115dda7f26a8d450783c60d63028ef1fcc3fb
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-06-28 17:34:53 -0500

    Section 6 - Villa Number Models

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaNumberCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaNumberCreateDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+
+        public string SpecialDetails { get; set; }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
