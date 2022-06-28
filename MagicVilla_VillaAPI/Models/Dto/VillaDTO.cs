87acdb22daa1175b0ce806b53540f0bcd5af67dc
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-06-28 10:27:43 -0500

    Section 4 - Entity Framework Core Database Models

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -8,7 +8,12 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,9 +1,18 @@
-﻿namespace MagicVilla_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
