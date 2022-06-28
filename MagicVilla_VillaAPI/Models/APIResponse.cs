d5c666c02e79ca0b0b1de79d48fdf4ca127e3c4d
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-06-28 17:07:58 -0500

    Section 6 - API Response

---
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
