a222c407265b7b06a55783d6d0bf0d15696d8845
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-04 13:10:26 -0500

    Section 7 - Setup MVC Web Project

---
+++ MagicVilla_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagicVilla_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
