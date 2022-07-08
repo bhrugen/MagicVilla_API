7a96cecd579c2b90d8ec6afedc8ec44f2ec4d648
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-08 16:42:23 -0500

    Section 7 - Display API Error Messages

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -62,6 +62,13 @@ namespace MagicVilla_Web.Controllers
                 {
                     return RedirectToAction(nameof(IndexVillaNumber));
                 }
+                else
+                {
+                    if (response.ErrorMessages.Count > 0)
+                    {
+                        ModelState.AddModelError("ErrorMessages", response.ErrorMessages.FirstOrDefault());
+                    }
+                }
             }
 
             var resp = await _villaService.GetAllAsync<APIResponse>();
