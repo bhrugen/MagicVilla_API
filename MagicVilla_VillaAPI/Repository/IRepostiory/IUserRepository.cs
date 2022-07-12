9d9882aecee860f5ae09befc5b240ea6d2ba33af
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-12 11:06:33 -0500

    Section 8 - Add User Repository

---
+++ MagicVilla_VillaAPI/Repository/IRepostiory/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepostiory
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepostiory;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
