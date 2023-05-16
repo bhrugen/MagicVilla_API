017f991aaa8af31c0423e748e682ad917feff5d6
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-16 12:27:49 -0500

    Register Page UI

--- MagicVilla_Identity/Pages/Account/Register/Index.cshtml.cs
+++ MagicVilla_Identity/Pages/Account/Register/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using MagicVilla_Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,39 @@ namespace MagicVilla_Identity.Pages.Account.Register
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager,
+                RoleManager<IdentityRole> roleInManager
+              )
         {
+            _roleManager = roleInManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+
+
+        public async Task<IActionResult> OnGet(string returnUrl)
+        {
+            List<string> roles = new()
+            {
+                SD.Admin,
+                SD.Customer
+            };
+            ViewData["roles_message"] = roles;
+            Input = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return Page();
         }
     }
 }
