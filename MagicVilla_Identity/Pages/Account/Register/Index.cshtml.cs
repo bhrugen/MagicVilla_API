8dffc8290316818bfbbb3a31b630d5e29d39ac40
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-16 12:30:29 -0500

    Register in Action

--- MagicVilla_Identity/Pages/Account/Register/Index.cshtml.cs
+++ MagicVilla_Identity/Pages/Account/Register/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using IdentityModel;
 using MagicVilla_Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace MagicVilla_Identity.Pages.Account.Register
 {
@@ -39,6 +41,67 @@ namespace MagicVilla_Identity.Pages.Account.Register
             {
                 ReturnUrl = returnUrl
             };
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    EmailConfirmed = true,
+                    Name = Input.Name,
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync(Input.RoleName).GetAwaiter().GetResult())
+                    {
+                        var userRole = new IdentityRole
+                        {
+                            Name = Input.RoleName,
+                            NormalizedName = Input.RoleName,
+
+                        };
+                        await _roleManager.CreateAsync(userRole);
+                    }
+                    await _userManager.AddToRoleAsync(user, Input.RoleName);
+
+
+                    await _userManager.AddClaimsAsync(user, new Claim[] {
+                        new Claim(JwtClaimTypes.Name,Input.Email),
+                        new Claim(JwtClaimTypes.Email,Input.Email),
+                        new Claim(JwtClaimTypes.Role,Input.RoleName)
+                    });
+
+                    var loginresult = await _signInManager.PasswordSignInAsync(
+                        Input.Email, Input.Password, false, lockoutOnFailure: true);
+
+                    if (loginresult.Succeeded)
+                    {
+                        if (Url.IsLocalUrl(Input.ReturnUrl))
+                        {
+                            return Redirect(Input.ReturnUrl);
+                        }
+                        else if (string.IsNullOrEmpty(Input.ReturnUrl))
+                        {
+                            return Redirect("~/");
+                        }
+                        else
+                        {
+                            throw new Exception("invalid return URL");
+                        }
+
+                    }
+
+                }
+
+            }
             return Page();
         }
     }
