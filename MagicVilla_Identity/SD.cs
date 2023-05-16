c3016ff135764de67d6a1bf1df9666b0535504ae
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-16 12:11:03 -0500

    Configure Identity Resources,Scopes and Clients

---
+++ MagicVilla_Identity/SD.cs
@@ -0,0 +1,57 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace MagicVilla_Identity
+{
+    public static class SD
+    {
+        public const string Admin = "admin";
+        public const string Customer = "customer";
+
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        public static IEnumerable<ApiScope> ApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("magic", "Magic Server"),
+                new ApiScope(name: "read",   displayName: "Read your data."),
+                new ApiScope(name: "write",  displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
+        }
+
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "service.client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1", "api2.read_only" }
+                },
+                new Client
+                {
+                    ClientId = "magic",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes = { "magic",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email
+                    },
+                    RedirectUris={ "https://localhost:7002/signin-oidc" },
+                    PostLogoutRedirectUris={"https://localhost:7002/signout-callback-oidc" },
+                }
+            };
+        }
+    }
+}
